5f9584c3aaa68c6227389fd30799d75b11075e54
Sebastien Allamand <sebastien@allamand.com>
2020-03-11 17:04:27 +0000

    mycommit

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World Go"}
+		res := &response{Message: "Hello World Go without Lambda"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
