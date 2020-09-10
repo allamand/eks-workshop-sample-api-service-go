75fbff539c573a56d2c559ef0721c8f304de8812
Sébastien Allamand <sallaman@amazon.com>
2020-09-10 15:29:31 +0200

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World Go without Lambda"}
+		res := &response{Message: "Hello World Go CdK"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
